d612f4ff3ae719a21383f3da194c552bcd13cfd7
Amol Raut <amolraut9991@gmail.com>
2025-04-19 16:48:01 +0530

    address issue

--- server/middlewares/authSeller.js
+++ server/middlewares/authSeller.js
@@ -1,25 +1,22 @@
 import jwt from 'jsonwebtoken';
 
-const authSeller = async ( req, res, next) => {
+const authSeller = async (req, res, next) => {
     const { sellerToken } = req.cookies;
 
     if (!sellerToken) {
-        return res.json({success: false, message: 'Not authorized'});
+        return res.json({ success: false, message: 'Not Authorized' });
     }
-
     try {
-
         const tokenDecode = jwt.verify(sellerToken, process.env.JWT_SECRET)
         if (tokenDecode.email === process.env.SELLER_EMAIL) {
             next();
+
+        } else {
+            return res.json({ success: false, message: 'Not Authorized' });
         }
-        else{
-            return res.json({success: false, message: 'Not authorized'});
-        }
- 
     } catch (error) {
-        res.json({success: false, message: error.message});
+        res.json({ success: false, message: error.message });
     }
 }
 
-export  default authSeller;
+export default authSeller;
--- server/middlewares/authUser.js
+++ server/middlewares/authUser.js
@@ -21,38 +21,3 @@ const authUser = async (req, res, next) => {
 }
 
 export default authUser
-
-/* import jwt from 'jsonwebtoken';
-
-//in this we will get cookies from the request so when we request any api endpoint in this req we will have cookies 
-//so from that cookies we will extract the token 
-
-const authUser = async ( req, res, next ) => { //next will execute the controller function
-
-    const { token } = req.cookies;
-
-    if (!token) {
-        return res.json({success: false, message: 'Not authorized'});
-    }
-    //if token is available then we will decode this token to extract the id
-    try {
-        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET)
-        if (tokenDecode.id) {
-            
-            if (!req.body) req.body = {}; // ✅ Add this line to avoid the error
-            req.body.userId = tokenDecode.id;
-        }
-        else{
-            return res.json({success: false, message: 'Not authorized'});
-        }
-
-        next();
-
-    } catch (error) {
-        res.json({success: false, message: error.message});
-    }
-} 
-
-export default authUser;
-
- */
